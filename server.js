7ba1fdef7b8613d71f34c69891a1f22c3fa35a74
Bastian Pethel <bastianpethel360@gmail.com>
2025-08-06 12:43:04 -0400

    established a route

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const express = require('express');
 
 const app = express();
 
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
